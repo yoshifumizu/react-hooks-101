46b9a5d02b007183c827c3105e8c468b5b8cf8df
Mizutani Yoshifumi <yosmizutani@abeam.com>
2021-03-21 17:50:53 +0900

    implement all events deletion feature

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,10 +3,13 @@ import React from 'react'
 const Event = ({ event, dispatch }) => {
     const id = event.id
     const handleClickDeleteButton = () => {
-      dispatch({
-        type: 'DELETE_EVENT',
-        id: id
-      })
+      const result = window.confirm(`イベント(id=${id})を本当に削除して良いですか？`)
+      if (result) {
+        dispatch({
+            type: 'DELETE_EVENT',
+            id: id
+          })
+        }
     }
 
     return (
