7ab7ffaa0c77c903069f66faed154c6cdb335c71
Mizutani Yoshifumi <yosmizutani@abeam.com>
2021-03-22 08:35:49 +0900

    setup react context provider

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import Event from './Event'
+import AppContext from '../contexts/AppContext'
 
 const Events = ({state, dispatch}) => {
+    const value = useContext(AppContext)
     return (
         <>
             <h4>イベント一覧</h4>
