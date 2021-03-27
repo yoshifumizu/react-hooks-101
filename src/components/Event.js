ce010f632ba48a5110070e63e8c174b408b9fb40
Mizutani Yoshifumi <yosmizutani@abeam.com>
2021-03-28 08:45:48 +0900

    create operation in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react'
 
-import { DELETE_EVENT } from '../actions'
+import { 
+  DELETE_EVENT,
+  ADD_OPERATION_LOG
+} from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils/utils'
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext)
@@ -11,6 +15,12 @@ const Event = ({ event }) => {
     const result = window.confirm(`イベント(id=${id})を本当に削除して良いですか？`)
     if (result) {
       dispatch({type: DELETE_EVENT, id})
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `イベント(id=${id})を削除ました。`,
+        operatedAt: timeCurrentIso8601()
+      })
     }
   }
 
